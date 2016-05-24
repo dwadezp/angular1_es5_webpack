6445ef88ef7a79c8cbbbdb6ac52e48e78c6305ae
vbezruch <vbezruch@cisco.com>
2016-05-24 13:20:08 +0300

    phantomJS: added phantomJS for unit testing
    Change-Id: I7b87d22d01e579a3209150de45b4ae86990cfab7

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
     files: [
       './specs.webpack.js'
     ],
+    browsers: ['PhantomJS'],
     reporters: ['progress', 'coverage'],
     preprocessors: {
       './specs.webpack.js': ['webpack']
